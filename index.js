054ffe8962ba8bcb6fe8b13ab27002829c134774
ANUSHKA <anushkamediratta>
2023-02-25 01:00:03 +0530

    Show user functionality

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app=express()
+const db=require('./model/connection')
+
+app.use(express.json())
+
+//Create user
+app.post("/adduser",(req,res)=>{
+const user={name:req.body.name,email:req.body.email,phone:req.body.mobile,city:req.body.city}
+let sql="INSERT INTO `employee` SET ?"
+db.query(sql,user,(err,result)=>{
+    if(err) throw err;
+    else res.json(result)
+})
+})
+
+//Show User
+app.get("/showUser",(req,res)=>{
+    let sql="SELECT * FROM `employee`"
+    db.query(sql,(err,result)=>{
+        if(err) throw err
+        else
+        res.json(result)
+    })
+})
+
+//Show a particular user
+app.get("/showUser/:email",(req,res)=>{
+    let sql=`SELECT * FROM employee WHERE email='${req.params.email}'`
+    db.query(sql,(err,result)=>{
+        if(err) throw err
+        else
+        res.json(result)
+    })
+})
+
+
+
+const PORT=process.env.PORT || 3000
+app.listen(PORT,()=>console.log(`Server is running at ${PORT}`))
